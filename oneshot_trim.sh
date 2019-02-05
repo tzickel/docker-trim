39fe4f97f4762a9b4a15a7a412f513fc9b301094
tzickel <tzickel@users.noreply.github.com>
2019-02-05 09:21:25 +0200

    some oneshot script fixes

--- oneshot_trim.sh
+++ oneshot_trim.sh
@@ -7,6 +7,7 @@ set -e
 
 [[ -z "$1" ]] && { echo "Please specifiy a docker image"; exit 1; }
 IMAGE_NAME="$1"
+IMAGE_NAME_ESCAPED="${1/\//_}"
 DOCKER_ARGS=${DOCKER_ARGS:--it --rm}
 shift
 # If image does not exist, pull it.
@@ -14,8 +15,8 @@ shift
 # Create an instrumentation temporary image
 echo "> Creating temporary instrumentation image"
 INSTRUMENT_IMAGE=`python docker_prep_instrument_image.py ${IMAGE_NAME}`
-echo "> Running image, press Ctrl-C when done (or finish the container)"
-STRACE_TMP_FILE="${IMAGE_NAME}.strace_tmp_file"
+echo "> Running image, press Ctrl-C when done (or finish the container, or kill it from another console)"
+STRACE_TMP_FILE="${IMAGE_NAME_ESCAPED}.strace_tmp_file"
 # TODO check for file existance (and not directory)
 touch ${STRACE_TMP_FILE}
 # Run the instrumentation image, and output the file access i/o to STRACE_TMP_FILE
@@ -23,11 +24,11 @@ set +e
 docker run ${DOCKER_ARGS} --cap-add=SYS_PTRACE --mount type=bind,source="`pwd`/${STRACE_TMP_FILE}",target=/tmp/strace_output ${INSTRUMENT_IMAGE} $@
 set -e
 echo "> Processing file access instrumentation"
-STRACE_PARSED_TMP_FILE="${IMAGE_NAME}.strace_parsed_tmp_file"
+STRACE_PARSED_TMP_FILE="${IMAGE_NAME_ESCAPED}.strace_parsed_tmp_file"
 # Parse the strace output into a file list
 python docker_parse_strace.py ${IMAGE_NAME} ${STRACE_TMP_FILE} > ${STRACE_PARSED_TMP_FILE}
 # Check out the file types being opened
-FILE_TMP_FILE="${IMAGE_NAME}.file_tmp_file"
+FILE_TMP_FILE="${IMAGE_NAME_ESCAPED}.file_tmp_file"
 docker run --entrypoint="" --mount type=bind,source="`pwd`/${STRACE_PARSED_TMP_FILE}",target=/tmp/parsed_output --rm ${INSTRUMENT_IMAGE} /tmp/instrumentation/file -m /tmp/instrumentation/magic.mgc -b -L -f /tmp/parsed_output > ${FILE_TMP_FILE}
 # Extract the ELF interpreter being used to run the apps
 python docker_parse_file.py ${FILE_TMP_FILE} >> ${STRACE_PARSED_TMP_FILE}
@@ -35,7 +36,7 @@ python docker_parse_file.py ${FILE_TMP_FILE} >> ${STRACE_PARSED_TMP_FILE}
 echo "> Removing temporary instrumentation image"
 docker rmi -f ${INSTRUMENT_IMAGE}
 # Parse the file list, and create the final file list
-FINAL_TMP_FILE="${IMAGE_NAME}.final_tmp_file"
+FINAL_TMP_FILE="${IMAGE_NAME_ESCAPED}.final_tmp_file"
 python docker_scan_image.py ${IMAGE_NAME} ${STRACE_PARSED_TMP_FILE} > ${FINAL_TMP_FILE}
 # Create the trimmed image
 echo "> Creating trimmed image"
